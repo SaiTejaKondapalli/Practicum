0a4d2f582dab7b41e256c7218b94dcc54a503838
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-06-14 18:36:52 +0530

    commit

---
+++ reactprogress/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Taskdisplay from './Taskdisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <Taskdisplay />
+    </div>
+  );
+}
+
+export default App;
