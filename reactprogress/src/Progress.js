6c233d4a84eddf67581a8f4631c99fff20b2f68f
SaiTejaKondapalli <saiteja.kondapalli@msitprogram.net>
2020-06-14 18:35:35 +0530

    added progress bar

---
+++ reactprogress/src/Progress.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ProgressBar } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Progressbar extends Component {
+    render() {
+        const now = Math.round((this.props.completed / this.props.total_days) * 100);
+        const progressInstance = <ProgressBar now={now} label={`${now}%`} />;
+        return (
+            <div style={{ width: 300}}>
+                {progressInstance}
+            </div>
+        );
+    }
+}
+
+export default Progressbar;
